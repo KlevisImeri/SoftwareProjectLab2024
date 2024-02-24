b79dba435bf066ae4e170cb9f77cc2c8b7219b89
Klevis Imeri <klevisimeri11@gmail.com>
2024-02-24 21:29:02 +0100

    Added Google JavaDoc style for VsCode formattring and for Maven to chechk while builind

--- src/main/java/mainstring/dev/Main.java
+++ src/main/java/mainstring/dev/Main.java
@@ -1,7 +1,15 @@
 package mainstring.dev;
 
+/**
+ * The Main class represents the entry point of the application.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  /*
+   * The main method is the entry point of the application.
+   * 
+   * @param args command-line arguments (not used in this application)
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
