de053fa7a9599cc68614e6d986a166d25da5213e
MeanDiamand <diamondmean09@gmail.com>
2024-05-20 00:20:22 +0200

    Adding the desert texture to the grid system of the game and decorating the name label of the players improved visibility

--- src/main/java/mainstring/dev/Players/Player/PlayerView.java
+++ src/main/java/mainstring/dev/Players/Player/PlayerView.java
@@ -2,6 +2,8 @@ package mainstring.dev.Players.Player;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import javax.swing.JLabel;
 import mainstring.dev.JImage;
@@ -10,7 +12,10 @@ import mainstring.dev.Elements.Element.ElementView;
 public class PlayerView extends JImage {
   JLabel name = new JLabel() {
     {
-      setForeground(new Color(227, 129, 54));
+        setForeground(new Color(227, 129, 54));
+        setOpaque(true);
+        setBackground(Color.BLACK);
+        setFont(new Font("Arial", Font.BOLD, 10)); 
     }
   };
 
@@ -20,9 +25,17 @@ public class PlayerView extends JImage {
     super(playerImage);
     this.location = location;
     name.setText(player.getName());
+    setLayout(null); // Use null layout to allow absolute positioning
     add(name);
     setPreferredSize(new Dimension(60, 60));
     setImageSize(getPreferredSize());
+
+    // Position the name label at the bottom center
+    FontMetrics fm = name.getFontMetrics(name.getFont());
+    int textWidth = fm.stringWidth(name.getText());
+    int labelHeight = fm.getHeight();
+    int x = (getPreferredSize().width - textWidth) / 2;
+    name.setBounds(x, getPreferredSize().height - labelHeight, textWidth, labelHeight);
 
     new PlayerController(player, this);
   }
